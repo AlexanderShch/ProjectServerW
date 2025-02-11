37eb8438c9a9d2e55347ea72b9a7f22036160b24
AlexanderShch <137498757+AlexanderShch@users.noreply.github.com>
2025-02-11 20:02:15 +0300

    Revert "Установлена библиотека libxlsxwriter"
    This reverts commit a5da0ed3bcb24d089192e1d904849763bd617d22.

--- Chart.cpp
+++ Chart.cpp
@@ -1,18 +1,1 @@
 #include "Chart.h"
-// Some older versions on Visual Studio may need "stdafx.h".
-// #include "stdafx.h"
-
-#include "xlsxwriter.h"
-
-int main() {
-
-    lxw_workbook* workbook = workbook_new("hello_world.xlsx");
-    lxw_worksheet* worksheet = workbook_add_worksheet(workbook, NULL);
-
-    worksheet_write_string(worksheet, 0, 0, "Hello", NULL);
-    worksheet_write_number(worksheet, 1, 0, 123, NULL);
-
-    workbook_close(workbook);
-
-    return 0;
-}
